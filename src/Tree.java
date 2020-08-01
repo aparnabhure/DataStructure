48977dff86bb8e428280b0c37cfb7078f44b7cf7
aparnabhure <aparnabhure1983@gmail.com>
2020-08-01 17:44:53 +0530

    Added tree class

---
+++ src/Tree.java
@@ -0,0 +1,46 @@
+public class Tree {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    static class Node
+    {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
+}
