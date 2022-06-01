c024e14aa05311d46fc27b084ca62b0660531e58
aparnabhure <aparnabhure1983@gmail.com>
2022-06-01 23:46:42 +0530

    Added PairExistsWithKSum

--- src/PairExistsWithKSum.java
+++ src/PairExistsWithKSum.java
@@ -1,5 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PairExistsWithKSum {
     /*
@@ -11,6 +11,42 @@ public class PairExistsWithKSum {
         System.out.println(pairExists(A, 14));
         System.out.println(pairExists(A, 4));
         System.out.println(pairExists(A, 18));
+
+        ArrayList<Integer> pair = twoSum(Arrays.stream(A).boxed().collect(Collectors.toList()), 14);
+        System.out.print(pair.isEmpty()?"[]":"["+pair.get(0)+", "+pair.get(1)+"]");
+    }
+
+    static ArrayList<Integer> twoSum(final List<Integer> A, int B) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+        int n = A.size();
+
+        Map<Integer, List<Integer>> freqMap = new HashMap<>();
+        for(int i=0; i<n; i++){
+            List<Integer> indexes = freqMap.getOrDefault(A.get(i), new ArrayList<>());
+            indexes.add(i);
+            freqMap.put(A.get(i), indexes);
+        }
+
+        for(int i =0;i<n; i++){
+            int diff = B-A.get(i);
+            if(freqMap.containsKey(diff)){
+                List<Integer> indexes = freqMap.get(diff);
+                if(diff == A.get(i)){
+                    if(indexes.size()>=2){
+                        result.add(i+1);
+                        result.add(indexes.get(1)+1);
+                        break;
+                    }
+                }else{
+                    result.add(i+1);
+                    result.add(indexes.get(0)+1);
+                    break;
+                }
+            }
+        }
+        return result;
+
     }
 
     //O(n)
