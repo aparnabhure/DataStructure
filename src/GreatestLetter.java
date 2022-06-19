2a3c13f3491f388ba1a3c293e317178583619eb0
Aparna Bhure <aparnabhure1983@gmail.com>
2022-06-19 20:42:35 +0530

    @Added GreatestLetter

---
+++ src/GreatestLetter.java
@@ -0,0 +1,61 @@
+//https://leetcode.com/contest/weekly-contest-298/problems/greatest-english-letter-in-upper-and-lower-case/
+/*
+Given a string of English letters s, return the greatest English letter which occurs as both a lowercase and uppercase letter in s. The returned letter should be in uppercase. If no such letter exists, return an empty string.
+
+An English letter b is greater than another letter a if b appears after a in the English alphabet.
+
+
+
+Example 1:
+
+Input: s = "lEeTcOdE"
+Output: "E"
+Explanation:
+The letter 'E' is the only letter to appear in both lower and upper case.
+Example 2:
+
+Input: s = "arRAzFif"
+Output: "R"
+Explanation:
+The letter 'R' is the greatest letter to appear in both lower and upper case.
+Note that 'A' and 'F' also appear in both lower and upper case, but 'R' is greater than 'F' or 'A'.
+Example 3:
+
+Input: s = "AbCdEfGhIjK"
+Output: ""
+Explanation:
+There is no letter that appears in both lower and upper case.
+ */
+public class GreatestLetter {
+    public static void main(String[] args) {
+
+        System.out.println(greatestLetter("lEeTcOdE"));
+        System.out.println(greatestLetter("arRAzFif"));
+
+    }
+
+    static String greatestLetter(String s) {
+
+        int[] smallCase = new int[26];
+        int[] capitalCase = new int[26];
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (Character.isLowerCase(c)) {
+                smallCase[c - 'a']++;
+            } else {
+                capitalCase[c - 'A']++;
+            }
+        }
+
+        for (int i = 25; i >= 0; i--) {
+            if (smallCase[i] != 0 && capitalCase[i] != 0) {
+                int value = 'A' + i;
+                return "" + (char) value;
+            }
+        }
+
+        return "";
+    }
+}
