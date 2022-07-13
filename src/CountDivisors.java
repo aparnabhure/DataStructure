eb457c22cf366dd4ac849fc84c9f58aeb9f3d4dc
aparnabhure <aparnabhure1983@gmail.com>
2022-07-13 23:33:42 +0530

    CountDivisors updated

--- src/CountDivisors.java
+++ src/CountDivisors.java
@@ -7,13 +7,57 @@ public class CountDivisors {
         print(result);
         result = solveSqrtTC(new int[]{2,3,4,5});
         print(result);
+        result = solveUsingPrimeFactorization(new int[]{2,3,4,5});
+        print(result);
     }
-
     static void print(int[] result){
         for(int i:result){
             System.out.print(i+ " ");
         }
         System.out.println();
+    }
+
+    static int[] solveUsingPrimeFactorization(int[] A){
+        int n = A.length;
+
+        int max = Integer.MIN_VALUE;
+        for(int i: A){
+            max = Math.max(i, max);
+        }
+
+        //prepare smallest prime factors array
+        int[] spf = new int[max+1];
+        for(int i=2; i<=max; i++){
+            spf[i]=i;
+        }
+        //Calculate smallest prime factors
+        int sqrt = (int)Math.sqrt(max);
+        for(int i=2; i<=sqrt; i++){
+            if(spf[i] == i){
+                for(int j=i*i; j<=max; j+=i){
+                    if(spf[j] == j){
+                        spf[j] = i;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            //for each query
+            int N = A[i];
+            int total =1;
+            while(N>1){
+                int p= spf[N];
+                int c = 0;
+                while (N%p == 0){
+                    c++;
+                    N = N/p;
+                }
+                total *= (1+c);
+            }
+            A[i] = total;
+        }
+        return A;
     }
     static int[] solve(int[] A){
         int n =A.length;
