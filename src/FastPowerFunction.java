7add642791e3a3fe33ab568c7c471b5850d8452b
Aparna Bhure <aparnabhure1983@gmail.com>
2022-07-25 09:50:08 +0530

    @Update FastPower to accept negative number and any MOD

--- src/FastPowerFunction.java
+++ src/FastPowerFunction.java
@@ -1,19 +1,26 @@
 public class FastPowerFunction {
     public static void main(String[] args) {
+        System.out.println(fastPower(71045970, 41535484,64735492));
+        System.out.println(fastPower(-1, 1, 20));
+
         int mod = (int)1e9+7;
         System.out.println(fastPower(10,5,mod));
         System.out.println(fastPower(5,3,mod));
+
+
     }
 
     static int fastPower(int A, int pow, int mod){
-        if(pow == 0)return 1;
+        if(A == 0) return 0;
+        if(pow == 0) return 1;
 
-        int p = fastPower(A, pow/2, mod) %mod;
-        p = (int)((p*(long)p)%mod);
+        long p = (long)fastPower(A, pow/2, mod) %mod;
+        p = ((p%mod)*(p%mod))%mod;
         if(pow%2 == 0){
-            return p;
+            return p<0?(int)(p+mod):(int)p;
         }
 
-        return ((int)(A*(long)p)%mod);
+        p = ((A%mod) *(p%mod))%mod;
+        return p<0?(int)(p+mod):(int)p;
     }
 }
