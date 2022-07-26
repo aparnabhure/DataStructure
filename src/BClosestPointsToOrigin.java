e4dbde688ff180234706f60dfa9a3a83e5900b42
Aparna Bhure <aparnabhure1983@gmail.com>
2022-07-26 18:03:14 +0530

    BClosestPointsToOrigin

---
+++ src/BClosestPointsToOrigin.java
@@ -0,0 +1,103 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BClosestPointsToOrigin {
+    /*
+    We have a list A of points (x, y) on the plane. Find the B closest points to the origin (0, 0).
+
+Here, the distance between two points on a plane is the Euclidean distance.
+
+You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in.)
+
+NOTE: Euclidean distance between two points P1(x1, y1) and P2(x2, y2) is sqrt( (x1-x2)2 + (y1-y2)2 ).
+
+
+
+Problem Constraints
+1 <= B <= length of the list A <= 105
+-105 <= A[i][0] <= 105
+-105 <= A[i][1] <= 105
+
+
+
+Input Format
+The argument given is list A and an integer B.
+
+
+
+Output Format
+Return the B closest points to the origin (0, 0) in any order.
+
+
+
+Example Input
+Input 1:
+
+ A = [
+       [1, 3],
+       [-2, 2]
+     ]
+ B = 1
+Input 2:
+
+ A = [
+       [1, -1],
+       [2, -1]
+     ]
+ B = 1
+
+
+Example Output
+Output 1:
+
+ [ [-2, 2] ]
+Output 2:
+
+ [ [1, -1] ]
+
+
+Example Explanation
+Explanation 1:
+
+ The Euclidean distance will be sqrt(10) for point [1,3] and sqrt(8) for point [-2,2].
+ So one closest point will be [-2,2].
+Explanation 2:
+
+ The Euclidean distance will be sqrt(2) for point [1,-1] and sqrt(5) for point [2,-1].
+ So one closest point will be [1,-1].
+     */
+    public static void main(String[] args) {
+        int[][] result = solve(new int[][]{{1,3},{-2,2}}, 1);
+        print(result);
+        result = solve(new int[][]{{1,-1},{2,-1}}, 1);
+        print(result);
+    }
+
+    static void print(int[][] result){
+        for(int[] i: result){
+            System.out.print(i[0] +" " + i[1]+ ", ");
+        }
+        System.out.println();
+    }
+
+    static int[][] solve(int[][] A, int B) {
+
+        Arrays.sort(A, (o1, o2) -> {
+            long x2 = (long) o1[0] *o1[0];
+            long y2 = (long) o1[1] *o1[1];
+            long X = x2+y2;
+            x2 = (long) o2[0] *o2[0];
+            y2 = (long) o2[1] *o2[1];
+            long Y = x2+y2;
+
+            return Long.compare(X, Y);
+        });
+
+        int[][] result = new int[B][2];
+        for(int i=0; i<B; i++){
+            result[i] = Arrays.copyOf(A[i], A[i].length);
+        }
+
+        return result;
+    }
+}
