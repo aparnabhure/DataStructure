d203de6eb98b2c87fd19e2a519425c4abb1b378f
aparnabhure <aparnabhure1983@gmail.com>
2022-08-04 13:14:34 +0530

    Added AllocateBooks

---
+++ src/AllocateBooks.java
@@ -0,0 +1,100 @@
+public class AllocateBooks {
+    /*
+    Given an array of integers A of size N and an integer B.
+
+The College library has N books. The ith book has A[i] number of pages.
+
+You have to allocate books to B number of students so that the maximum number of pages allocated to a student is minimum.
+
+A book will be allocated to exactly one student.
+Each student has to be allocated at least one book.
+Allotment should be in contiguous order, for example: A student cannot be allocated book 1 and book 3, skipping book 2.
+Calculate and return that minimum possible number.
+
+NOTE: Return -1 if a valid assignment is not possible.
+
+
+
+Problem Constraints
+1 <= N <= 105
+1 <= A[i], B <= 105
+
+
+
+Input Format
+The first argument given is the integer array A.
+The second argument given is the integer B.
+
+
+
+Output Format
+Return that minimum possible number.
+
+
+
+Example Input
+A = [12, 34, 67, 90]
+B = 2
+
+
+Example Output
+113
+
+
+Example Explanation
+There are two students. Books can be distributed in following fashion :
+
+1)  [12] and [34, 67, 90]
+    Max number of pages is allocated to student 2 with 34 + 67 + 90 = 191 pages
+2)  [12, 34] and [67, 90]
+    Max number of pages is allocated to student 2 with 67 + 90 = 157 pages
+3)  [12, 34, 67] and [90]
+    Max number of pages is allocated to student 1 with 12 + 34 + 67 = 113 pages
+    Of the 3 cases, Option 3 has the minimum pages = 113.
+     */
+
+    public static void main(String[] args) {
+        System.out.println(books(new int[]{31, 14, 19, 75}, 12));
+        System.out.println(books(new int[]{10, 8, 7, 1, 5, 5, 10, 10, 1, 5, 3}, 17));
+        System.out.println(books(new int[]{12, 34, 67, 90},2));
+    }
+
+    static int books(int[] A, int B) {
+        if(B > A.length) return -1;
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+        for(int i:A){
+            low = Math.max(low, i);
+            high += i;
+        }
+
+        int ans = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(isValid(A, mid, B)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    static boolean isValid(int[] A, int mid, int B){
+        int count = 1;
+        int sum = 0;
+        for(int i: A){
+            if((sum + i) <= mid){
+                sum += i;
+            }else if(count<B){
+                count++;
+                sum = i;
+            }else{
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
