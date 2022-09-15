6122f8d73ce8cc910537189f5683c47e54c93076
aparnabhure <aparnabhure1983@gmail.com>
2022-09-15 20:47:19 +0530

    Updated serialization

--- src/DeserializeTree.java
+++ src/DeserializeTree.java
@@ -1,15 +1,21 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class DeserializeTree {
     //Level order array given
     public static void main(String[] args) {
         TreeNode root =deserialize(Arrays.asList(1, 2, 3, 4, 5, -1, -1, -1, -1, -1, -1));
         System.out.println(root.val);
+        print(serialize(root));
         root =deserialize(Arrays.asList(1, 2, 3, 4, 5, -1, 6, -1, -1, -1, -1, -1, -1));
         System.out.println(root.val);
+        print(serialize(root));
+    }
+
+    static void print(ArrayList<Integer> result){
+        for(int i:result){
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 
     static TreeNode deserialize(List<Integer> A){
@@ -32,5 +38,22 @@ public class DeserializeTree {
         }
 
         return root;
+    }
+
+    static ArrayList<Integer> serialize(TreeNode A) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(A != null){
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(A);
+            while(!queue.isEmpty()){
+                TreeNode node = queue.poll();
+                ans.add(node==null?-1:node.val);
+                if(node != null) {
+                    queue.add(node.left);
+                    queue.add(node.right);
+                }
+            }
+        }
+        return ans;
     }
 }
