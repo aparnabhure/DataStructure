ed7e044bb1907c6911af2f9e5472842a803ebe53
aparnabhure <aparnabhure1983@gmail.com>
2022-09-23 23:48:36 +0530

    BuildTreeFromInorderPostOrder updated

--- src/BuildTreeFromInOrderPostOrder.java
+++ src/BuildTreeFromInOrderPostOrder.java
@@ -1,12 +1,36 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class BuildTreeFromInOrderPostOrder {
     public static void main(String[] args) {
         TreeNode root = buildTree(Arrays.asList(6, 1, 3, 2), Arrays.asList(6, 3, 2, 1));
+        TreeNode.printTreeArray(TreeNode.postorderTraversal(root));
         TreeNode.printTreeArray(TreeNode.inorderTraversal(root));
+        TreeNode.printTreeArray(TreeNode.preorderTraversal(root));
+
+        List<Integer> inorder = Arrays.asList(6, 1, 3, 2);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<inorder.size(); i++){
+            map.put(inorder.get(i), i);
+        }
+        List<Integer> postorder = Arrays.asList(6, 3, 2, 1);
+        root = build(postorder,0, 3, 0, map);
+        TreeNode.printTreeArray(TreeNode.postorderTraversal(root));
+        TreeNode.printTreeArray(TreeNode.inorderTraversal(root));
+        TreeNode.printTreeArray(TreeNode.preorderTraversal(root));
+    }
+
+    static TreeNode build(List<Integer> postorder, int pleft, int pright, int ileft, Map<Integer, Integer> map){
+        if(pleft>pright){
+            return null;
+        }
+
+        int val = postorder.get(pright);
+        TreeNode node = new TreeNode(val);
+        int idx = map.get(val);
+        int length = idx-ileft;
+        node.left = build(postorder, pleft, pleft+length-1, ileft, map);
+        node.right = build(postorder, pleft+length, pright-1, idx+1, map);
+        return node;
     }
 
     static int index=0;
