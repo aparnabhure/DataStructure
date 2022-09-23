eb9201ab4067d5ec49bb57d1a9960e04a14f35c6
aparnabhure <aparnabhure1983@gmail.com>
2022-09-23 23:32:59 +0530

    BuildTreeFromInorderPreOrder updated

--- src/BuildTreeFromInorderPreOrder.java
+++ src/BuildTreeFromInorderPreOrder.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class BuildTreeFromInorderPreOrder {
     public static void main(String[] args) {
@@ -12,6 +10,17 @@ public class BuildTreeFromInorderPreOrder {
         TreeNode.printTreeArray(TreeNode.postorderTraversal(root1));
         TreeNode.printTreeArray(TreeNode.inorderTraversal(root1));
         TreeNode.printTreeArray(TreeNode.preorderTraversal(root1));
+
+        List<Integer> inorder = Arrays.asList(6, 1, 3, 2);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<inorder.size(); i++){
+            map.put(inorder.get(i), i);
+        }
+        List<Integer> preorder = Arrays.asList(1, 6, 2, 3);
+        TreeNode root = build(preorder,0, 3, 0, map);
+        TreeNode.printTreeArray(TreeNode.postorderTraversal(root));
+        TreeNode.printTreeArray(TreeNode.inorderTraversal(root));
+        TreeNode.printTreeArray(TreeNode.preorderTraversal(root));
     }
 
     static int index=0;
@@ -19,6 +28,20 @@ public class BuildTreeFromInorderPreOrder {
         if (inorder == null || preorder == null || (inorder.size() != preorder.size())) return null;
         int n = preorder.size();
         return build(inorder, preorder, index, n-1);
+    }
+
+    static TreeNode build(List<Integer> preorder, int pleft, int pright, int ileft, Map<Integer, Integer> map){
+        if(pleft>pright){
+            return null;
+        }
+
+        int val = preorder.get(pleft);
+        TreeNode node = new TreeNode(val);
+        int idx = map.get(val);
+        int length = idx-ileft;
+        node.left = build(preorder, pleft+1, pleft+length, ileft, map);
+        node.right = build(preorder, pleft+length+1, pright, idx+1, map);
+        return node;
     }
 
     static TreeNode build(List<Integer> inorder, List<Integer> preorder, int left, int right) {
