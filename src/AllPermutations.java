52d6d4199c77bff22f38a9a75a3a28d5d3dc1da3
aparnabhure <aparnabhure1983@gmail.com>
2022-10-13 20:32:19 +0530

    AllPermutations

---
+++ src/AllPermutations.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class AllPermutations {
+    public static void main(String[] args) {
+        print(permute(new ArrayList<>(Arrays.asList(1,2,3))));
+    }
+
+    static void print(ArrayList<ArrayList<Integer>> result){
+        for(ArrayList<Integer> i: result){
+            for(int j:i){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A) {
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        permute(A, A.size(), 0, new ArrayList<>(A), result);
+        return result;
+    }
+
+    static void permute(ArrayList<Integer>A, int n, int index, ArrayList<Integer> permutation, ArrayList<ArrayList<Integer>> result){
+        if(index >= n){
+            result.add(new ArrayList<>(permutation));
+            return;
+        }
+
+        for(int j=index; j<n; j++){
+            Collections.swap(permutation, j, index);
+            permute(A, n, index+1, permutation, result);
+            Collections.swap(permutation, j, index);
+        }
+    }
+}
